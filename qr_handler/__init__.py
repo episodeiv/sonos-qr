83ac22cacc6b9300c8bf3f068c7ef811b7e319a5
Dennis Lichtenthäler <dennis@episode-iv.de>
2020-06-08 22:50:04 +0200

    Some more boilerplate

--- qr_handler/__init__.py
+++ qr_handler/__init__.py
@@ -1,8 +1,9 @@
 import re
+import requests
 import time
 
 valid_code = re.compile('^sonos-qr:([a-z0-9_]+)&([a-z0-9_]+)', re.IGNORECASE)
-same_code_timoeut = 1.0
+same_code_timeout = 1.0
 previous_code = None
 
 
@@ -11,9 +12,27 @@ def handle_code(content):
 		return False
 
 	if(content == previous_code):
-		time.sleep(same_code_timoeut)
+		time.sleep(same_code_timeout)
 		return False
 
+	match = re.search(valid_code, content)
+
+	action = match.group(1)
+	token = match.group(2)
+
+	result = make_request(action, token)
+
+	return result
+
+
+
+def make_request(action, token):
+	r = requests.get("https://maker.ifttt.com/trigger/" + action + "/with/key/" + token)
+
+	if r.status_code == 200:
+		return True
+	else:
+		return False
 
 
 
